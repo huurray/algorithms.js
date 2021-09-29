f477d433c9df78d73e5bbf8bea2c1b59ce680c62
Ray(허준혁) <ray.heo@bunjang.co.kr>
2021-09-29 16:15:33 +0900

    About LinkedList

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import LinkedList from "./utils/LinkedList";
+import LinkedList from "./concepts/LinkedList";
 
-const linkedList = new LinkedList(3);
-
-linkedList.getValue();
+const linkedList = new LinkedList(1);
+console.log(linkedList.push(2));
+console.log(linkedList.push(3));
+console.log("linkedList", linkedList);
