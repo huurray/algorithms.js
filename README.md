e11c07c7dd7b5142ef189304d99a00e833b07834
Ray(허준혁) <ray.heo@bunjang.co.kr>
2021-10-07 17:26:09 +0900

    Hash Table

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Huurray's algorithms.
+# Data structures implemented in JavaScript
+
+from Udemy lecture ( Data Structures & Algorithms - JavaScript by Scott Barrett )
+
+URL : https://www.udemy.com/course/data-structures-algorithms-javascript/
---
+++ src/utils/array.js
@@ -0,0 +1,11 @@
+// 교집합이 있는지
+function itemInCommon(arr1, arr2) {
+  let obj = {};
+  for (let i = 0; i < arr1.length; i++) {
+    obj[arr1[i]] = true;
+  }
+  for (let j = 0; j < arr2.length; j++) {
+    if (obj[arr2[j]]) return true;
+  }
+  return false;
+}
