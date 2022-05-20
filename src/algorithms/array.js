9ee015d98e8fe410045735a008de4d68874c66e4
Ray(허준혁) <ray.heo@bunjang.co.kr>
2022-05-20 17:39:36 +0900

    imp: 함수 추가

--- src/algorithms/array.js
+++ src/algorithms/array.js
@@ -1,5 +1,5 @@
 // 교집합이 있는지
-export function itemInCommon(arr1, arr2) {
+function itemInCommon(arr1, arr2) {
   let obj = {};
   for (let i = 0; i < arr1.length; i++) {
     obj[arr1[i]] = true;
@@ -8,4 +8,19 @@ export function itemInCommon(arr1, arr2) {
     if (obj[arr2[j]]) return true;
   }
   return false;
+}
+
+// 조합
+function getCombinations(arr, selectNumber) {
+  const results = [];
+  if (selectNumber === 1) return arr.map((el) => [el]);
+
+  arr.forEach((fixed, index, origin) => {
+    const rest = origin.slice(index + 1);
+    const combinations = getCombinations(rest, selectNumber - 1);
+    const attached = combinations.map((el) => [fixed, ...el]);
+    results.push(...attached);
+  });
+
+  return results;
 }
--- src/algorithms/number.js
+++ src/algorithms/number.js
@@ -1,5 +1,31 @@
 // 팩토리얼 함수
-export function factorial(n) {
+function factorial(n) {
   if (n === 1) return 1;
   return n * factorial(n - 1);
+}
+
+/**
+ * 소수 찾기
+ * @return boolean;
+ */
+function isPrime(num) {
+  for (let i = 2, s = Math.sqrt(num); i <= s; i++) {
+    if (num % i === 0) return false;
+  }
+  return num > 1;
+}
+
+/**
+ * 약수 찾기
+ * @return number[];
+ */
+function getDivisors(num) {
+  const divisors = [];
+  for (let i = 1; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) {
+      divisors.push(i);
+      if (num / i != i) divisors.push(num / i);
+    }
+  }
+  return divisors;
 }
