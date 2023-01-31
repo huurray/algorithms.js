35f2351e38258b926be91dd6bb2d03f0af10e8d5
Ray(허준혁) <ray.heo@bunjang.co.kr>
2023-01-31 10:58:04 +0900

    imp: 2022년 알고리즘 보완

--- playground/src/index.js
+++ playground/src/index.js
@@ -1,11 +1,20 @@
-function* solution(max) {
-  let index = 0;
-  let sum = 0;
-  while (index < max) {
-    index++;
-    sum += index;
-    yield sum;
+function solution(script) {
+  const lines = script.toString().trim().split(/\n/g);
+  const inputs = lines[0].split(" ").map(Number);
+  let h = inputs[0];
+  let m = inputs[1];
+
+  if (h === 0 && m < 45) {
+    console.log(`23 ${60 - (45 - m)}`);
+    return;
   }
+
+  const minutes = h * 60 + m - 45;
+  console.log(`${Math.floor(minutes / 60)} ${minutes % 60}`);
 }
 
-console.log("result", solution(3));
+// solution(require("fs").readFileSync("/dev/stdin"));
+
+solution(`
+0 0
+`);
